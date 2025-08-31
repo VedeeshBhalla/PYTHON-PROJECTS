65f355200770f0e57f11739d4ba0ca081030a273
Vedeesh Bhalla <vedeesh.bhalla12@gmail.com>
2025-08-31 11:46:06 +0530

    new changes

--- Project/StonePaperScissor.py
+++ Project/StonePaperScissor.py
@@ -1,29 +1,33 @@
 import random
 
 options = ["stone" , "paper" , "scissors"]
+running = True
 
-playerName = input("Enter player name : ")
-computer = random.choice(options)
-playerOptions = None
+while running:
+        playerName = input("Enter player name : ")
+        computer = random.choice(options)
+        playerOptions = None
 
-
-while playerOptions not in options:
-    playerOptions = input("Enter a choice(stone , paper , scissors) : ").lower()
-
-if   playerOptions== computer:
-     print("ITS A TIE !!")  
-elif playerOptions == "stone" and computer == "scissors":
-    print(f"{playerName} wins!!") 
-elif playerOptions == "stone" and computer == "paper":
-    print(f"Computer wins!!")
-elif playerOptions == "s" and computer == "paper":
-    print(f"{playerName} wins!!")
-elif playerOptions == "s" and computer == "stone":
-    print(f"Computer wins!!")
-elif playerOptions == "paper" and computer == "stone":
-    print(f"{playerName} wins!!")
-elif playerOptions == "paper" and computer == "s":
-    print("Computer wins!!")
+        while playerOptions not in options:
+            playerOptions = input("Enter a choice(stone , paper , scissors) : ").lower()
+            print("------------------------------------------------------")
+            if   playerOptions== computer:
+                print("ITS A TIE !!")  
+            elif playerOptions == "stone" and computer == "scissors":
+                print(f"{playerName} wins!!") 
+            elif playerOptions == "stone" and computer == "paper":
+                print(f"Computer wins!!")
+            elif playerOptions == "s" and computer == "paper":
+                print(f"{playerName} wins!!")
+            elif playerOptions == "s" and computer == "stone":
+                print(f"Computer wins!!")
+            elif playerOptions == "paper" and computer == "stone":
+                print(f"{playerName} wins!!")
+            elif playerOptions == "paper" and computer == "s":
+                print("Computer wins!!")
+            else:
+                break   
     
-print(f"{playerName} choice : {playerOptions}")
-print(f"Computer choice : {computer}")
+        print(f"{playerName} choice : {playerOptions}")
+        print(f"Computer choice : {computer}")
+        print("------------------------------------------------------")
