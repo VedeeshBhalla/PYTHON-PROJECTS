20c7c1cdc19566232f986ef4052e11560b789ae8
Vedeesh Bhalla <vedeesh.bhalla12@gmail.com>
2025-08-31 14:45:08 +0530

    Modified

--- Project/StonePaperScissor.py
+++ Project/StonePaperScissor.py
@@ -2,32 +2,45 @@ import random
 
 options = ["stone" , "paper" , "scissors"]
 running = True
-
+playerName = input("Enter player name : ")
+win = 0
+total = 0
 while running:
-        playerName = input("Enter player name : ")
         computer = random.choice(options)
         playerOptions = None
-
+        
         while playerOptions not in options:
             playerOptions = input("Enter a choice(stone , paper , scissors) : ").lower()
             print("------------------------------------------------------")
             if   playerOptions== computer:
-                print("ITS A TIE !!")  
+                print("ITS A TIE !!") 
+                total+=1 
             elif playerOptions == "stone" and computer == "scissors":
                 print(f"{playerName} wins!!") 
+                wins+=1
+                total+=1 
             elif playerOptions == "stone" and computer == "paper":
                 print(f"Computer wins!!")
-            elif playerOptions == "s" and computer == "paper":
+                total+=1 
+            elif playerOptions == "scissors" and computer == "paper":
                 print(f"{playerName} wins!!")
-            elif playerOptions == "s" and computer == "stone":
+                wins+=1
+                total+=1 
+            elif playerOptions == "scissors" and computer == "stone":
                 print(f"Computer wins!!")
+                total+=1 
             elif playerOptions == "paper" and computer == "stone":
                 print(f"{playerName} wins!!")
+                wins+=1
+                total+=1 
             elif playerOptions == "paper" and computer == "s":
                 print("Computer wins!!")
+                total+=1 
             else:
                 break   
     
         print(f"{playerName} choice : {playerOptions}")
         print(f"Computer choice : {computer}")
         print("------------------------------------------------------")
+        
+print(f"Score = {wins}/{total}")
